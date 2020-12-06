64b5fa9074d6893b7b02f28915aa23079855af7e
ThePoisonSlayer <75578461+ThePoisonSlayer@users.noreply.github.com>
2020-12-06 21:02:02 +0300

    Sim

---
+++ Calculator Simple.py
@@ -0,0 +1,12 @@
+print("\n\n\n\n\t\t\t\t\tCALCULATOR\n\n\n")
+
+x = int(input("Enter the first number:-"))
+y = int(input("Enter the second number:-"))
+print(x,"added to",y,"is",x+y)
+print(y,"subtracted from",x,"is",x-y)
+print(x,"multiplied by",y,"is",x*y)
+print(x,"divided by",y,"is",x/y)
+print("The remainder",x%y,"remains when you divide",x,"by",y)
+print(x,"to the power of",y,"is",x**y)
+print("\n\n\n\n\nEpic Calculations Procedure developed by Pratham Virani")
+
